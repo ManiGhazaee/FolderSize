b5dc38a58c2c80dbfea767e73525c0d262c3f4b3
ManiGhazaee <hosseinghazaee1@gmail.com>
2024-04-22 22:58:35 +0330

    refactor: improve code readability

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,17 +3,18 @@ use serde::{Deserialize, Serialize};
 use std::{
     collections::HashMap,
     fs::read_dir,
+    os::windows::fs::MetadataExt,
     path::{Path, PathBuf},
-    sync::{Arc, RwLock},
+    sync::{Arc, Mutex, RwLock},
     time::{Duration, Instant},
 };
+use tauri::{Runtime, Window};
 
 #[macro_use]
 extern crate lazy_static;
 
 lazy_static! {
-    pub static ref MAP: Arc<RwLock<HashMap<PathBuf, u64>>> =
-        Arc::new(RwLock::new(HashMap::new()));
+    pub static ref MAP: Arc<RwLock<HashMap<PathBuf, u64>>> = Arc::new(RwLock::new(HashMap::new()));
 }
 
 pub struct Timer {
@@ -58,29 +59,97 @@ pub struct Entries {
     entries: Vec<(u64, PathBuf, bool)>,
 }
 
-impl Entries {
-    pub fn new(root: &PathBuf, size: u64, map: &MAP) -> Self {
-        let entries = match read_dir(root) {
+pub struct X<R: Runtime> {
+    root: PathBuf,
+    timer: Arc<Mutex<Timer>>,
+    size: Arc<RwLock<u64>>,
+    window: Arc<Window<R>>,
+    map: &'static MAP,
+}
+
+impl<R: Runtime> X<R> {
+    pub fn run(root: PathBuf, window: Window<R>) -> u64 {
+        let s = Self {
+            root: root.clone(),
+            timer: Arc::new(Mutex::new(Timer::new(Duration::from_millis(400)))),
+            size: Arc::new(RwLock::new(0)),
+            window: Arc::new(window),
+            map: &MAP,
+        };
+        *s.size.write().unwrap() = s.recurse(&root);
+        let entries = s.entries();
+        s.window.emit("entries", entries).unwrap();
+        let x = *s.size.read().unwrap();
+        x
+    }
+    fn recurse(&self, path: &Path) -> u64 {
+        if path.is_file() {
+            let size = size_of(path);
+            *self.size.write().unwrap() += size;
+            if self.timer.lock().unwrap().tick() {
+                self.window
+                    .emit("sum-size", *self.size.read().unwrap())
+                    .unwrap();
+            }
+            size
+        } else if path.is_dir() {
+            let path_buf = path.to_path_buf();
+            if self.map.read().unwrap().contains_key(&path_buf) {
+                *self.map.read().unwrap().get(&path_buf).unwrap()
+            } else {
+                let size = match read_dir(path) {
+                    Ok(rd) => rd
+                        .into_iter()
+                        .par_bridge()
+                        .map(|entry| {
+                            let path = entry.unwrap().path();
+                            if self.map.read().unwrap().contains_key(&path) {
+                                *self.map.read().unwrap().get(&path).unwrap()
+                            } else {
+                                let size = self.recurse(&path);
+                                self.map.write().unwrap().insert(path, size);
+                                size
+                            }
+                        })
+                        .sum(),
+                    _ => 0,
+                };
+                self.map.write().unwrap().insert(path_buf, size);
+                size
+            }
+        } else {
+            0
+        }
+    }
+    pub fn entries(&self) -> Entries {
+        let entries = match read_dir(self.root.clone()) {
             Ok(rd) => rd
                 .into_iter()
                 .par_bridge()
                 .map(|entry| {
                     let path = entry.unwrap().path();
                     let is_file = path.is_file();
-                    (*map.read().unwrap().get(&path).unwrap(), path, is_file)
+                    (*self.map.read().unwrap().get(&path).unwrap(), path, is_file)
                 })
                 .collect(),
             _ => Default::default(),
         };
-        let mut parent = root.parent().unwrap_or(&Path::new("")).to_path_buf();
+        let mut parent = self.root.parent().unwrap_or(&Path::new("")).to_path_buf();
         if !MAP.read().unwrap().contains_key(&parent) {
             parent = Path::new("").to_path_buf();
         }
-        Self {
-            size,
-            root: root.to_owned(),
+        Entries {
+            size: *self.size.read().unwrap(),
+            root: self.root.to_owned(),
             parent,
             entries,
         }
+    }
+}
+
+pub fn size_of(path: &Path) -> u64 {
+    match path.metadata() {
+        Ok(m) => m.file_size(),
+        _ => 0,
     }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,93 +1,19 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{
-    fs::read_dir,
-    os::windows::fs::MetadataExt,
-    path::Path,
-    sync::{Arc, Mutex, RwLock},
-    time::{Duration, Instant},
-};
+use std::{path::Path, time::Instant};
 
-use file_size::{Entries, Timer, MAP};
-use jwalk::rayon::iter::{ParallelBridge, ParallelIterator};
+use file_size::X;
 use tauri::Runtime;
 
 #[tauri::command]
 async fn folder_size<R: Runtime>(window: tauri::Window<R>, path: String) -> u64 {
     let inst = Instant::now();
-    let path_str = Path::new(&path).to_path_buf();
-    let timer = Arc::new(Mutex::new(Timer::new(Duration::from_millis(400))));
-    let sum_size: Arc<RwLock<u64>> = Arc::new(RwLock::new(0));
-    let window = Arc::new(window);
-    let size = recurse_size_map(Arc::clone(&window), &path_str, &MAP, sum_size, timer);
 
-    let entries = Entries::new(&path_str, size, &MAP);
-
-    window.emit("entries", entries).unwrap();
+    let size = X::run(Path::new(&path).to_path_buf(), window);
 
     dbg!(inst.elapsed());
 
     size
-}
-
-fn recurse_size_map<'a, R: Runtime>(
-    window: Arc<tauri::Window<R>>,
-    path: &Path,
-    map: &MAP,
-    sum_size: Arc<RwLock<u64>>,
-    timer: Arc<Mutex<Timer>>,
-) -> u64 {
-    if path.is_file() {
-        let size = file_size(path);
-
-        *sum_size.write().unwrap() += size;
-
-        if timer.lock().unwrap().tick() {
-            window.emit("sum-size", *sum_size.read().unwrap()).unwrap();
-        }
-
-        size
-    } else if path.is_dir() {
-        let path_buf = path.to_path_buf();
-        if map.read().unwrap().contains_key(&path_buf) {
-            *map.read().unwrap().get(&path_buf).unwrap()
-        } else {
-            let size = match read_dir(path) {
-                Ok(rd) => rd
-                    .into_iter()
-                    .par_bridge()
-                    .map(|entry| {
-                        let path = entry.unwrap().path();
-                        if map.read().unwrap().contains_key(&path) {
-                            *map.read().unwrap().get(&path).unwrap()
-                        } else {
-                            let size = recurse_size_map(
-                                Arc::clone(&window),
-                                &path,
-                                &map,
-                                Arc::clone(&sum_size),
-                                Arc::clone(&timer),
-                            );
-                            map.write().unwrap().insert(path, size);
-                            size
-                        }
-                    })
-                    .sum(),
-                _ => 0,
-            };
-            map.write().unwrap().insert(path_buf, size);
-            size
-        }
-    } else {
-        0
-    }
-}
-
-fn file_size(path: &Path) -> u64 {
-    match path.metadata() {
-        Ok(m) => m.file_size(),
-        _ => 0,
-    }
 }
 
 fn main() {
